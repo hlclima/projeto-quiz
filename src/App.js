e7248d11b93ca0df5cb8a6649a9a72f1aa9c116e
Hugo Lima <hugo.lima_thoughtworks@prestador.globo>
2023-05-13 21:11:53 -0300

    incluir botao de confirmacao

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
 
 const questions = [
   {
@@ -10,54 +9,74 @@ const questions = [
   },
   {
     question: 'Qual é a capital de PE?',
-    options: ['Jaboatao', 'Olinda', 'Recife', 'Caruaru'],
+    options: ['Jaboatão', 'Olinda', 'Recife', 'Caruaru'],
     correctAnswerIndex: 2
   },
   {
-    question: 'Qual é a formula da agua?',
+    question: 'Qual é a fórmula da água?',
     options: ['O2', 'NaCl', 'CO2', 'H2O'],
     correctAnswerIndex: 3
-  },
+  }
   // Adicione mais perguntas aqui...
 ];
 
 const App = () => {
   const [questionIndex, setQuestionIndex] = useState(0);
   const [correctAnswers, setCorrectAnswers] = useState(0);
+  const [fade, setFade] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [disableOptions, setDisableOptions] = useState(false);
+
+  useEffect(() => {
+    setFade(false);
+    setSelectedOption(null);
+    setDisableOptions(false);
+  }, [questionIndex]);
 
   const handleOptionSelect = (selectedOptionIndex) => {
-    if (selectedOptionIndex === questions[questionIndex].correctAnswerIndex) {
-      setCorrectAnswers(correctAnswers + 1);
+    setSelectedOption(selectedOptionIndex);
+  };
+
+  const handleConfirmClick = () => {
+    if (selectedOption === questions[questionIndex].correctAnswerIndex) {
+      setCorrectAnswers((prevCorrectAnswers) => prevCorrectAnswers + 1);
     }
-    setQuestionIndex(questionIndex + 1);
+    setDisableOptions(true);
+    setFade(true);
+    setTimeout(() => {
+      setQuestionIndex((prevQuestionIndex) => prevQuestionIndex + 1);
+      setDisableOptions(false);
+    }, 500);
   };
 
   return (
-    <div className="App">
-      {questionIndex < questions.length ? (
-        <div className="container">
-          <h1>Questão {questionIndex + 1}</h1>
-          <div className="question">{questions[questionIndex].question}</div>
-          <div className="options">
-            {questions[questionIndex].options.map((option, index) => (
-              <div
-                className="option"
-                key={index}
-                onClick={() => handleOptionSelect(index)}
-              >
-                {option}
-              </div>
-            ))}
-          </div>
+    <div className={`App ${fade ? 'fade-out' : ''}`}>
+      <div className="container">
+        <h1>{questionIndex < questions.length ? `Questão ${questionIndex + 1}` : 'Resultado'}</h1>
+        <div className="question">
+          {questionIndex < questions.length ? questions[questionIndex].question : `Você acertou ${correctAnswers} de ${questions.length} questões.`}
         </div>
-      ) : (
-        <div className="container">
-          <h1>Resultado</h1>
-          <div className="result">
-            Você acertou {correctAnswers} de {questions.length} questões.
-          </div>
+        <div className="options">
+          {questionIndex < questions.length && questions[questionIndex].options.map((option, index) => (
+            <div
+              className={`option ${selectedOption === index ? 'selected' : ''}`}
+              key={index}
+              onClick={() => handleOptionSelect(index)}
+            >
+              {option}
+            </div>
+          ))}
         </div>
-      )}
+        {questionIndex < questions.length && (
+          <button
+            className="confirm-button"
+            onClick={handleConfirmClick}
+            disabled={disableOptions || selectedOption === null}
+          >
+            Confirmar
+          </button>
+        )}
+      </div>
     </div>
   );
 };
