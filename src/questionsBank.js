3d0310fd8ad8c6811c8bc1fed99483ce1749edea
Hugo Lima <hugo.lima_thoughtworks@prestador.globo>
2023-05-14 15:51:09 -0300

    arquivo com banco de questoes

---
+++ src/questionsBank.js
@@ -0,0 +1,55 @@
+//import { questionsProva1, questionsProva2, questionsProva3 } from './questions'; // Importar as listas de perguntas
+export const questionsProva1 = [
+  {
+    question: 'Qual é a capital do Brasil?',
+    options: ['Brasília', 'Rio de Janeiro', 'São Paulo', 'Salvador'],
+    correctAnswerIndex: 0,
+  },
+  {
+    question: 'Qual é a capital de PE?',
+    options: ['Jaboatão', 'Olinda', 'Recife', 'Caruaru'],
+    correctAnswerIndex: 2,
+  },
+  {
+    question: 'Qual é a fórmula da água?',
+    options: ['O2', 'NaCl', 'CO2', 'H2O'],
+    correctAnswerIndex: 3,
+  },
+  // Adicione mais perguntas aqui...
+];
+export const questionsProva2 = [
+  {
+    question: 'Quantos títulos mundiais a seleção brasileira tem?',
+    options: ['2', '3', '4', '5'],
+    correctAnswerIndex: 3,
+  },
+  {
+    question: 'Qual o mês em que se comemora o São João?',
+    options: ['Abril', 'Junho', 'Julho', 'Outubro'],
+    correctAnswerIndex: 1,
+  },
+  {
+    question: 'Quantas rodas possui o carrinho de sorvete da kibom?',
+    options: ['0', '1', '2', '3'],
+    correctAnswerIndex: 2,
+  },
+  // Adicione mais perguntas aqui...
+];
+export const questionsProva3 = [
+  {
+    question: 'Qual o dia do aniversário de Catarina',
+    options: ['6 de junho', '19 de janeiro', '10 de novembro', '15 de junho'],
+    correctAnswerIndex: 2,
+  },
+  {
+    question: 'Qual o nome do bairro que Catarina mora?',
+    options: ['Torre', 'Madalena', 'Boa Viagem', 'Várzea'],
+    correctAnswerIndex: 0,
+  },
+  {
+    question: 'Qual é a idade de Catarina?',
+    options: ['18 anos', '10 anos', '2 anos', '8 anos'],
+    correctAnswerIndex: 3,
+  },
+  // Adicione mais perguntas aqui...
+];
