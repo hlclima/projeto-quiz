7a836ae29f892196bfc67b25e2bf27432b43bf04
Hugo Lima <hugo.lima_thoughtworks@prestador.globo>
2023-05-13 22:25:17 -0300

    adicionada navegacao entre as perguntas

--- src/App.js
+++ src/App.js
@@ -1,65 +1,80 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const questions = [
   {
     question: 'Qual é a capital do Brasil?',
     options: ['Brasília', 'Rio de Janeiro', 'São Paulo', 'Salvador'],
-    correctAnswerIndex: 0
+    correctAnswerIndex: 0,
   },
   {
     question: 'Qual é a capital de PE?',
     options: ['Jaboatão', 'Olinda', 'Recife', 'Caruaru'],
-    correctAnswerIndex: 2
+    correctAnswerIndex: 2,
   },
   {
     question: 'Qual é a fórmula da água?',
     options: ['O2', 'NaCl', 'CO2', 'H2O'],
-    correctAnswerIndex: 3
-  }
+    correctAnswerIndex: 3,
+  },
   // Adicione mais perguntas aqui...
 ];
 
 const App = () => {
   const [questionIndex, setQuestionIndex] = useState(0);
-  const [correctAnswers, setCorrectAnswers] = useState(0);
-  const [fade, setFade] = useState(false);
-  const [selectedOption, setSelectedOption] = useState(null);
-  const [disableOptions, setDisableOptions] = useState(false);
-
-  useEffect(() => {
-    setFade(false);
-    setSelectedOption(null);
-    setDisableOptions(false);
-  }, [questionIndex]);
+  const [answers, setAnswers] = useState(new Array(questions.length).fill(null));
+  const [showResult, setShowResult] = useState(false);
 
   const handleOptionSelect = (selectedOptionIndex) => {
-    setSelectedOption(selectedOptionIndex);
+    const updatedAnswers = [...answers];
+    updatedAnswers[questionIndex] = selectedOptionIndex;
+    setAnswers(updatedAnswers);
   };
 
-  const handleConfirmClick = () => {
-    if (selectedOption === questions[questionIndex].correctAnswerIndex) {
-      setCorrectAnswers((prevCorrectAnswers) => prevCorrectAnswers + 1);
-    }
-    setDisableOptions(true);
-    setFade(true);
-    setTimeout(() => {
-      setQuestionIndex((prevQuestionIndex) => prevQuestionIndex + 1);
-      setDisableOptions(false);
-    }, 500);
+  const handleSubmit = () => {
+    setShowResult(true);
   };
+
+  if (showResult) {
+    // Contar as respostas corretas
+    let correctAnswers = 0;
+    answers.forEach((answer, index) => {
+      if (answer === questions[index].correctAnswerIndex) {
+        correctAnswers++;
+      }
+    });
+
+    return (
+      <div className="App">
+        <div className="container">
+          <h1>Resultado</h1>
+          <div className="result">
+            Você acertou {correctAnswers} de {questions.length} questões.
+          </div>
+          <div className="answer-summary">
+            <h2>Respostas:</h2>
+            {questions.map((question, index) => (
+              <p key={index}>
+                <strong>{question.question}</strong>
+                <br />
+                Sua resposta: {question.options[answers[index]]}
+              </p>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
-    <div className={`App ${fade ? 'fade-out' : ''}`}>
+    <div className="App">
       <div className="container">
-        <h1>{questionIndex < questions.length ? `Questão ${questionIndex + 1}` : 'Resultado'}</h1>
-        <div className="question">
-          {questionIndex < questions.length ? questions[questionIndex].question : `Você acertou ${correctAnswers} de ${questions.length} questões.`}
-        </div>
+        <h1>Questão {questionIndex + 1}</h1>
+        <div className="question">{questions[questionIndex].question}</div>
         <div className="options">
-          {questionIndex < questions.length && questions[questionIndex].options.map((option, index) => (
+          {questions[questionIndex].options.map((option, index) => (
             <div
-              className={`option ${selectedOption === index ? 'selected' : ''}`}
+              className={`option ${answers[questionIndex] === index ? 'selected' : ''}`}
               key={index}
               onClick={() => handleOptionSelect(index)}
             >
@@ -67,15 +82,17 @@ const App = () => {
             </div>
           ))}
         </div>
-        {questionIndex < questions.length && (
-          <button
-            className="confirm-button"
-            onClick={handleConfirmClick}
-            disabled={disableOptions || selectedOption === null}
-          >
-            Confirmar
-          </button>
-        )}
+        <div className="navigation-buttons">
+          {questionIndex > 0 && (
+            <button onClick={() => setQuestionIndex(questionIndex - 1)}>Anterior</button>
+          )}
+          {questionIndex < questions.length - 1 && (
+            <button onClick={() => setQuestionIndex(questionIndex + 1)}>Próxima</button>
+          )}
+          {questionIndex === questions.length - 1 && (
+            <button onClick={handleSubmit}>Submeter</button>
+          )}
+        </div>
       </div>
     </div>
   );
